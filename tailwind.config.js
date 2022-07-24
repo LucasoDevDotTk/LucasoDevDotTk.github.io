fd6ec3bf6cd6fe317788daa3a005735f3c8a45b5
Lucas Nguyen <lucasoAlt@outlook.com>
2022-07-24 19:30:41 +0200

    basic code, implement tailwind and typewriter.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        MainBg: "#050b39",
+        primary: "#8a06d7",
+        primary1: "#bc0dde",
+        primary2: "#ee387a",
+        contrast: "#080fc6",
+      },
+    },
+  },
+  plugins: [],
+};
