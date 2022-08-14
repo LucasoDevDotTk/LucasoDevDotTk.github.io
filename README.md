d90dcabb5e5ca43dcb9809ddd222889371772eb6
Lucas Nguyen <lucasoAlt@outlook.com>
2022-08-14 19:59:39 +0200

    REMOVE ALL OLD THOINGS

--- README.md
+++
@@ -1,7 +0,0 @@
-# My Website
-
-This is my website, built with tailwindcss and html.
-
-Development version in `<repository name>/dev`.
-
-Made with ❤ by LucasoDev.
--- js/typewriter.js
+++
@@ -1,9 +0,0 @@
-new Typewriter('#typewriter', {
-  strings: ['beautiful websites', 'amazing dashboards', 'easy-to-use apps', 'useful cli tools', 'discord bots'],
-  autoStart: true,
-  loop: true,
-  delay: 150,
-  pauseFor: 2500,
-});
-
-console.log("typewriter.js file ran");
