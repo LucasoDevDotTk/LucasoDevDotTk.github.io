a01470d498af0ae8b90fbe4c9a309617210d55dc
Lucas Nguyen <lucasoAlt@outlook.com>
2022-08-21 19:08:03 +0200

    add typewriter effect to componement "Typewriter.astro"

---
+++ src/js/typewriter.js
@@ -0,0 +1,9 @@
+new Typewriter('#typewriter', {
+  strings: ['beautiful websites', 'amazing dashboards', 'easy-to-use apps', 'useful cli tools', 'discord bots'],
+  autoStart: true,
+  loop: true,
+  delay: 150,
+  pauseFor: 2500,
+});
+
+console.log("typewriter.js file ran");
